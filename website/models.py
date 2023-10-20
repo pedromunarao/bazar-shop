9691711913636ebc8ff97ce4f33087118dbcc8c2
pedromunarao <munarao.pedro@gmail.com>
2023-10-20 11:35:53 -0300

    First Commit

---
+++ website/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+from django.db.models import Model
+from django.utils import timezone
+
+# Create your models here.
+
+LISTA_CATEGORIAS = (
+    ("SUPERIOR", "Parte Superior"),
+    ("INFERIOR", "Parte Inferior"),
+    ("UNICA", "Peça Única"),
+    ("TER_PECA", "Terceira Peça"),
+    ("ACESSORIOS", "Acessórios"),
+    ("CALÇADO", "Calçado"),
+    ("OUTROS", "Outros"),
+)
+
+
+LISTA_SUBCATEGORIAS = (
+    ("CAMISETA", "Camiseta"),
+    ("REGATA", "Regata"),
+    ("CROPPED", "Cropped"),
+    ("CAMISA", "Camisa"),
+    ("SHORTS", "Shorts"),
+    ("SAIA", "Saia"),
+    ("CALCA", "Calça"),
+)
+
+
+class Produto(models.Model):
+    titulo = models.CharField(max_length=100)
+    thumb = models.ImageField(upload_to='img_produto')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=100, choices=LISTA_CATEGORIAS)
+    subcategoria = models.CharField(max_length=100, choices=LISTA_SUBCATEGORIAS)
+    data_criacao = models.DateTimeField(default=timezone.now)
+    preco = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.titulo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
