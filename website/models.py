129dc5b7e64d02891bace2f277d1b7b0d236a652
pedromunarao <munarao.pedro@gmail.com>
2023-10-23 22:21:32 -0300

    2310

--- website/models.py
+++ website/models.py
@@ -14,24 +14,32 @@ LISTA_CATEGORIAS = (
     ("OUTROS", "Outros"),
 )
 
-
+'''
 LISTA_SUBCATEGORIAS = (
     ("CAMISETA", "Camiseta"),
     ("REGATA", "Regata"),
     ("CROPPED", "Cropped"),
     ("CAMISA", "Camisa"),
+    ("MOLETOM", "Moletom"),
+    ("VESTIDO", "Vestido"),
+    ("MACACAO", "Macacão"),
     ("SHORTS", "Shorts"),
     ("SAIA", "Saia"),
     ("CALCA", "Calça"),
+    ("ANEL", "Anel"),
+    ("PULSEIRA", "Pulseira"),
+    ("COLAR", "Colar"),
+    ("BRINCO", "Brinco"),
+    ("TENIS", "Tênis"),
 )
-
+'''
 
 class Produto(models.Model):
     titulo = models.CharField(max_length=100)
     thumb = models.ImageField(upload_to='img_produto')
     descricao = models.TextField(max_length=1000)
     categoria = models.CharField(max_length=100, choices=LISTA_CATEGORIAS)
-    subcategoria = models.CharField(max_length=100, choices=LISTA_SUBCATEGORIAS)
+    # subcategoria = models.CharField(max_length=100, choices=LISTA_SUBCATEGORIAS)
     data_criacao = models.DateTimeField(default=timezone.now)
     preco = models.DecimalField(max_digits=5, decimal_places=2)
 
