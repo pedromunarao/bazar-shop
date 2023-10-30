ead1e42d9e2c538e813809e66c090dbfa7530ddd
pedromunarao <111526170+pedromunarao@users.noreply.github.com>
2023-10-30 16:25:58 -0300

    login-ok

--- website/views.py
+++ website/views.py
@@ -1,20 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
     return render(request, "index.html")
 
-
+@login_required(login_url="/login")
 def shop(request):
     return render(request, "shop.html")
 
 
 def cadastro(request):
-    return render(request, "cadastro.html")
+    if request.method == "GET":
+        return render(request, "cadastro.html")
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('password')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('Usuário já existente')
+
+        user = User.objects.create_user(username=username, email=email, password=senha)
+        user.save()
+
+        return HttpResponse('Usuário Cadastrado com Sucesso')
 
 
 def login(request):
-    return render(request, "login.html")
+    if request.method == "GET":
+        return render(request, "login.html")
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('password')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request, user)
+            return render(request, "index.html")
+        else:
+            return HttpResponse('Usuário ou Senha Inválidos')
 
 
 
