890ab0b7b3672de5966e0220fc10631bcdf38b7f
pedromunarao <munarao.pedro@gmail.com>
2023-10-31 22:16:03 -0300

    login-semifinished

--- website/views.py
+++ website/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.decorators import login_required
 def index(request):
     return render(request, "index.html")
 
-@login_required(login_url="/login")
+
 def shop(request):
     return render(request, "shop.html")
 
